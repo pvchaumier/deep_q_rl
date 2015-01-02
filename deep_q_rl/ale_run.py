6bc5c6960b7fe7bb372f0f616634de571a5b82c3
Alejandro Dubrovsky <alito@organicrobot.com>
2015-01-03 01:40:25 +1100

    Parametrise initial runner

--- deep_q_rl/ale_run.py
+++ deep_q_rl/ale_run.py
@@ -12,13 +12,21 @@ import argparse
 DefaultBaseROMPath = "/usr/src/machinelearning/Arcade-Learning-Environment/roms/"
 DefaultROM = 'breakout.bin'
 DefaultPort = 4096
-
+DefaultStepsPerEpoch = 50000
+DefaultEpochs = 100
+DefaultStepsPerTest = 10000
 
 def main(args):
     # Check for glue_port command line argument and set it up...
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-r', '--rom', dest="rom", default=DefaultROM,
                         help='ROM to run (default: %(default)s)')
+    parser.add_argument('-e', '--epochs', dest="epochs", type=int, default=DefaultEpochs,
+                        help='Number of training epochs (default: %(default)s)')
+    parser.add_argument('-s', '--steps-per-epoch', dest="steps_per_epoch", type=int, default=DefaultStepsPerEpoch,
+                        help='Number of steps per epoch (default: %(default)s)')
+    parser.add_argument('-t', '--test-length', dest="test_steps", type=int, default=DefaultStepsPerTest,
+                        help='Number of steps per test (default: %(default)s)')    
     parser.add_argument('--glue-port', dest="glue_port", type=int, default=DefaultPort,
                         help='rlglue port (default: %(default)s)')
     parameters, unknown = parser.parse_known_args(args)
@@ -32,9 +40,10 @@ def main(args):
 
     # Start the necessary processes:
     p1 = subprocess.Popen(['rl_glue'], env=my_env, close_fds=close_fds)
-    p2 = subprocess.Popen(['ale', '-game_controller', 'rlglue', '-frame_skip', '2', '-disable_color_averaging','true', full_rom_path],
+    p2 = subprocess.Popen(['ale', '-game_controller', 'rlglue', '-frame_skip', '4', '-disable_color_averaging','true', full_rom_path],
                           env=my_env, close_fds=close_fds)
-    p3 = subprocess.Popen(['./rl_glue_ale_experiment.py', '-vv', '-s', '50000', '-t', '10000'], env=my_env, close_fds=close_fds)
+    p3 = subprocess.Popen(['./rl_glue_ale_experiment.py', '-vv', '--steps-per-epoch', str(parameters.steps_per_epoch), 
+        '--test-length', str(parameters.test_steps), '--epochs', str(parameters.epochs)], env=my_env, close_fds=close_fds)
     p4 = subprocess.Popen(['./rl_glue_ale_agent.py', '-vv'] + unknown, env=my_env, close_fds=close_fds)
 
     p1.wait()
