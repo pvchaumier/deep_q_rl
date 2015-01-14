92344d7c9a95b12b2ee49d023ad42d5b0468f24f
Alejandro Dubrovsky <alito@organicrobot.com>
2015-01-14 11:55:41 +1100

    Option not to plot the q-values

--- deep_q_rl/plot_results.py
+++ deep_q_rl/plot_results.py
@@ -12,23 +12,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(filename, game_name):
+def plot(filename, plot_q_values, game_name):
     # Modify this to do some smoothing...
     kernel = np.array([1.] * 1)
     kernel = kernel / np.sum(kernel)
 
     input_file = open(filename, "rb")
     results = np.loadtxt(input_file, delimiter=",", skiprows=1)
-    scores = plt.subplot(1, 2, 1)
+
+    if plot_q_values:
+        count = 2
+    else:
+        count = 1
+
+    scores = plt.subplot(1, count, 1)
     plt.plot(results[:, 0], np.convolve(results[:, 3], kernel, mode='same'), '-*')
     scores.set_xlabel('epoch')
     scores.set_ylabel('score')
 
+    if plot_q_values:
+        qvalues = plt.subplot(1, 2, 2)
+        plt.plot(results[:, 0], results[:, 5], '--')
+        qvalues.set_xlabel('epoch')
+        qvalues.set_ylabel('Q value')
 
-    qvalues = plt.subplot(1, 2, 2)
-    plt.plot(results[:, 0], results[:, 5], '--')
-    qvalues.set_xlabel('epoch')
-    qvalues.set_ylabel('Q value')
+    else:
+        scores.set_title(game_name)
 
 
     input_file.close()
@@ -43,12 +52,14 @@ def main(args):
     parser = ArgumentParser(description=__doc__)
     parser.add_argument("-g", "--game-name", dest="game_name", default=None,
         help="Name of game to put on title")
+    parser.add_argument("--no-q", dest="plotQValues", default=True, action="store_false",
+        help="Don't plot the Q values")    
     parser.add_argument("results", nargs=1,
         help="Results file")
 
     parameters = parser.parse_args(args)
 
-    plot(os.path.expanduser(parameters.results[0]), parameters.game_name)
+    plot(os.path.expanduser(parameters.results[0]), parameters.plotQValues, parameters.game_name)
 
     return 0
 
