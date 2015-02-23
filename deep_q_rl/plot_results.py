2ff3da488a2b4861eecf53692ba6c2e25a325572
Alejandro Dubrovsky <alito@organicrobot.com>
2015-02-24 00:57:09 +1100

    Optionally plot max scores gotten

--- deep_q_rl/plot_results.py
+++ deep_q_rl/plot_results.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(filename, plot_q_values, game_name):
+def plot(filename, plot_q_values, plot_max_values, game_name):
     # Modify this to do some smoothing...
     kernel = np.array([1.] * 1)
     kernel = kernel / np.sum(kernel)
@@ -20,23 +20,42 @@ def plot(filename, plot_q_values, game_name):
     input_file = open(filename, "rb")
     results = np.loadtxt(input_file, delimiter=",", skiprows=1)
 
-    if plot_q_values:
-        count = 2
-    else:
-        count = 1
+    plot_count = 1
 
-    scores = plt.subplot(1, count, 1)
+    if plot_q_values:
+        plot_count += 1
+
+    if plot_max_values:
+        plot_count += 1
+
+    scores = plt.subplot(1, plot_count, 1)
     plt.plot(results[:, 0], np.convolve(results[:, 3], kernel, mode='same'), '-*')
     scores.set_xlabel('epoch')
     scores.set_ylabel('score')
 
+    current_sub_plot = 2
+
+    if plot_max_values:
+        
+        max_values = plt.subplot(1, plot_count, current_sub_plot)
+        current_sub_plot += 1
+        plt.plot(results[:, 0], results[:, 4], 'r-.')
+        max_values.set_xlabel('epoch')
+        max_values.set_ylabel('Max score')
+        y_limits = max_values.get_ylim()
+        # set main score's limits to be the same as this one to make comparison easier
+        scores.set_ylim(y_limits)
+
     if plot_q_values:
-        qvalues = plt.subplot(1, 2, 2)
-        plt.plot(results[:, 0], results[:, 5], '--')
+        qvalues = plt.subplot(1, plot_count, current_sub_plot)
+        current_sub_plot += 1
+        plt.plot(results[:, 0], results[:, 5], '-')
         qvalues.set_xlabel('epoch')
         qvalues.set_ylabel('Q value')
 
-    elif game_name:
+
+
+    if game_name and plot_count == 1:
         scores.set_title(game_name)
 
 
@@ -54,12 +73,14 @@ def main(args):
         help="Name of game to put on title")
     parser.add_argument("--no-q", dest="plotQValues", default=True, action="store_false",
         help="Don't plot the Q values")    
+    parser.add_argument("--no-max", dest="plotMaxValues", default=True, action="store_false",
+        help="Don't plot the max values")        
     parser.add_argument("results", nargs=1,
         help="Results file")
 
     parameters = parser.parse_args(args)
 
-    plot(os.path.expanduser(parameters.results[0]), parameters.plotQValues, parameters.game_name)
+    plot(os.path.expanduser(parameters.results[0]), parameters.plotQValues, parameters.plotMaxValues, parameters.game_name)
 
     return 0
 
