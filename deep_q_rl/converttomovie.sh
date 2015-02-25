569bccffa102e3e68861b68201b9c225d5ce17b6
Alejandro Dubrovsky <alito@organicrobot.com>
2015-02-25 19:59:09 +1100

    Parameter to modify frames per second

--- deep_q_rl/converttomovie.sh
+++ deep_q_rl/converttomovie.sh
@@ -4,6 +4,13 @@ then
    output=output.mp4
 else
    output=$1
+   if [ $# -lt 2 ]
+   then
+      fps=15
+   else
+      fps=$2
+   fi
 fi
-avconv -r 15 -i frame%06d.png $output
+
+avconv -r $fps -i frame%06d.png $output
 
