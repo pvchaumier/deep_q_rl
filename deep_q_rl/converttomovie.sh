befc5b122b0ca1e90872893c699e5a1274bc1867
Alejandro Dubrovsky <alito@organicrobot.com>
2015-02-26 00:34:02 +1100

    remember about default fps

--- deep_q_rl/converttomovie.sh
+++ deep_q_rl/converttomovie.sh
@@ -4,12 +4,12 @@ then
    output=output.mp4
 else
    output=$1
-   if [ $# -lt 2 ]
-   then
-      fps=15
-   else
-      fps=$2
-   fi
+fi
+if [ $# -lt 2 ]
+then
+   fps=15
+else
+   fps=$2
 fi
 
 avconv -r $fps -i frame%06d.png $output
