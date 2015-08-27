6338522392680b740f888afa147dae47f2c0ec42
Alejandro Dubrovsky <alito@organicrobot.com>
2015-08-28 01:37:30 +1000

    Nicer interface to ale_run_watch

--- deep_q_rl/ale_run_watch.py
+++ deep_q_rl/ale_run_watch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """ This script runs a pre-trained network with the game
 visualization turned on.
 
@@ -7,18 +8,36 @@ ale_run_watch.py NETWORK_PKL_FILE [ ROM ]
 """
 import subprocess
 import sys
+import argparse
 
-def run_watch():
+DefaultROM = 'breakout'
+DefaultTestLength = 10000
+
+def run_watch(args):
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-r', '--rom', dest="rom", default=DefaultROM,
+                        help='ROM to run (default: %(default)s)')
+    parser.add_argument('-t', '--test-length', dest="steps_per_test",
+                        type=int, default=DefaultTestLength,
+                        help='Number of steps per test (default: %(default)s)')
+    parser.add_argument('networkfile', nargs=1,
+                        help='Network file')
+    parameters = parser.parse_args(args)
+
+
     command = ['./run_nature.py', '--steps-per-epoch', '0',
-               '--test-length', '10000', '--nn-file', sys.argv[1],
+               '--test-length', str(parameters.steps_per_test), '--nn-file', parameters.networkfile[0],
                '--display-screen']
 
-    if len(sys.argv) > 2:
-        command.extend(['--rom', sys.argv[2]])
+    if len(args) > 1:
+        command.extend(['--rom', parameters.rom])
 
     p1 = subprocess.Popen(command)
     
     p1.wait()
 
+    return 0
+
 if __name__ == "__main__":
-    run_watch()
+    sys.exit(run_watch(sys.argv[1:]))
