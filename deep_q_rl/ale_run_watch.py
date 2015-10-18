95bc5f4375325687a28433e99ea8cf6071e98b20
Alejandro Dubrovsky <alito@organicrobot.com>
2015-10-18 21:03:39 +1100

    Reduce ale_run_watch to turning on the screen and setting epochs to 0. Pass on all other variables

--- deep_q_rl/ale_run_watch.py
+++ deep_q_rl/ale_run_watch.py
@@ -2,36 +2,24 @@
 """ This script runs a pre-trained network with the game
 visualization turned on.
 
-Usage:
-
-ale_run_watch.py NETWORK_PKL_FILE [ ROM ]
+Specify the network file first, then any other options you want
 """
 import subprocess
 import sys
 import argparse
 
-DefaultROM = 'breakout'
-DefaultTestLength = 10000
 
 def run_watch(args):
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-r', '--rom', dest="rom", default=DefaultROM,
-                        help='ROM to run (default: %(default)s)')
-    parser.add_argument('-t', '--test-length', dest="steps_per_test",
-                        type=int, default=DefaultTestLength,
-                        help='Number of steps per test (default: %(default)s)')
     parser.add_argument('networkfile', nargs=1,
                         help='Network file')
-    parameters = parser.parse_args(args)
+    parameters, unknown = parser.parse_known_args(args)
 
 
     command = ['./run_nature.py', '--steps-per-epoch', '0',
-               '--test-length', str(parameters.steps_per_test), '--nn-file', parameters.networkfile[0],
-               '--display-screen']
-
-    if len(args) > 1:
-        command.extend(['--rom', parameters.rom])
+               '--nn-file', parameters.networkfile[0],
+               '--display-screen'] + unknown
 
     p1 = subprocess.Popen(command)
     
